69db793f591ab1deaab47d3e9ea41319e9b19ec9
PaulaEPR <paulaperera@gmail.com>
2022-02-02 12:55:37 +0100

    continue fetch

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,12 +1,12 @@
 const callToApi = (data) => {
-  return fetch('https://awesome-profile-cards.herokuapp.com/card', {
+  return fetch('https://localhost:4000/card', {
     method: 'POST',
     body: JSON.stringify(data),
     headers: { 'Content-Type': 'application/json' },
   })
     .then((response) => response.json())
-    .then((response) => {
-      return response;
+    .then((data) => {
+      return data;
     });
 };
 
